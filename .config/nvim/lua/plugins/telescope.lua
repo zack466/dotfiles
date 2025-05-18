118617f6f4386d035afb19140e654e91e85d8827
Zachary Huang <55601738+zack466@users.noreply.github.com>
2025-05-18 00:51:50 -0700

    telescope binding to open selected files in new buffers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,31 @@
- return {
+return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         defaults = {
-            file_ignore_patterns = { "^.git/", "^node_modules/" }
+            file_ignore_patterns = { "^.git/", "^node_modules/" },
+            mappings = {
+                i = {
+                    ["<C-o>"] = function(prompt_bufnr)
+                        -- open all files in quickfix list in new buffers
+                        require('telescope.actions').send_selected_to_qflist(prompt_bufnr, " ")
+                        vim.cmd('silent cfdo edit')
+                    end,
+                },
+                n = {
+                    ["<C-o>"] = function(prompt_bufnr)
+                        -- open all files in quickfix list in new buffers
+                        require('telescope.actions').send_selected_to_qflist(prompt_bufnr, " ")
+                        vim.cmd('silent cfdo edit')
+                    end,
+                },
+            },
         },
         pickers = {
             live_grep = {
                 additional_args = { "--hidden" }
             }
-        }
+        },
+
     }
 }
